e4c9e15b0f77f6a10102d602c38e987824a73df4
Colin Bruin <colinjb23@gmail.com>
2023-02-10 14:40:23 -0700

    adding auth0, login and logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
+    useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Button variant="contained" onClick={() => loginWithRedirect()}>
+        Sign Up
+      </Button>
+      <Button variant="contained" onClick={() => logout()}>
+        Logout
+      </Button>
+      {isAuthenticated ? <div>{user!.name}</div> : <div>Not Loaded</div>}
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="teralocker.us.auth0.com"
+      clientId="84AfgSeYZ3hYLjGA0jLUR1Nuafewzpjj"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
