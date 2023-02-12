6093688fcf9cb111ef58bc979f5b0e9d11afe60a
Colin Bruin <colinjb23@gmail.com>
2023-02-12 11:03:22 -0700

    adding a header barto application"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import "./App.css";
 
 function App() {
@@ -12,13 +18,30 @@ function App() {
 
   return (
     <div>
-      <Button variant="contained" onClick={() => loginWithRedirect()}>
-        Sign Up
-      </Button>
-      <Button variant="contained" onClick={() => logout()}>
-        Logout
-      </Button>
-      {isAuthenticated && user ? <div>{user.name}</div> : <div>Not Loaded</div>}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              TeraLocker
+            </Typography>
+            <Button variant="contained" onClick={() => loginWithRedirect()}>
+              Sign Up
+            </Button>
+            <Button variant="contained" onClick={() => logout()}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   );
 }
