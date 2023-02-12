769ea7337fdc13f745c4ec87ceedc99f17164c28
Colin Bruin <colinjb23@gmail.com>
2023-02-12 11:38:37 -0700

    adding prettier config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,51 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import "./App.css";
-import BurgerMenu from "./components/menu";
+import { useAuth0 } from '@auth0/auth0-react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import './App.css'
+import BurgerMenu from './components/menu'
 
 function App() {
-  const { loginWithRedirect, logout, isLoading } = useAuth0();
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+    const { loginWithRedirect, logout, isLoading } = useAuth0()
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
-  return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <BurgerMenu></BurgerMenu>
+    return (
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <BurgerMenu></BurgerMenu>
 
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              TeraLocker
-            </Typography>
-            <Button variant="contained" onClick={() => loginWithRedirect()}>
-              Sign Up
-            </Button>
-            <div className="logout-button">
-              <Button variant="contained" onClick={() => logout()}>
-                Logout
-              </Button>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </div>
-  );
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            TeraLocker
+                        </Typography>
+                        <Button
+                            variant="contained"
+                            onClick={() => loginWithRedirect()}
+                        >
+                            Sign Up
+                        </Button>
+                        <div className="logout-button">
+                            <Button
+                                variant="contained"
+                                onClick={() => logout()}
+                            >
+                                Logout
+                            </Button>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,17 +1,30 @@
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Menu } from "@mui/material";
 
 function BurgerMenu() {
+  const open = false;
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
   return (
-    <IconButton
-      size="large"
-      edge="start"
-      color="inherit"
-      aria-label="menu"
-      sx={{ mr: 2 }}
-    >
-      <MenuIcon />
-    </IconButton>
+    <div>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        aria-controls={open ? "demo-positioned-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu open={open}></Menu>
+    </div>
   );
 }
 
